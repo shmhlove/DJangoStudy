19616a32d744c99fc7f3aea7776bea473ff220e2
이상호 <shmhlove@naver.com>
2018-05-28 23:53:39 +0900

    [DJango] URL conf 문제 확인 중

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+#from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    #url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls)
+    #path('polls/', include('polls.urls')),
+    #path('admin/', admin.site.urls)
 ]
